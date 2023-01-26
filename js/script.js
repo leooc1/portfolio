2dc8a971c8f1f8d4f5aa82bb0c4c2dd591be80c8
leooc1 <leonardoocvrb@gmail.com>
2023-01-26 00:05:05 -0300

    botões criados e menu atualizado

--- js/script.js
+++ js/script.js
@@ -5,6 +5,23 @@ function menu(sanduiche){
         menu.style.width = '0px'
         menu.style.height = '0px'
         sanduiche.src = '../src/image/menu-line.svg'
+
+        document.querySelector('.esc-home').style.fontSize = '0'
+        document.querySelector('.esc-home').style.width = '0'
+        document.querySelector('.esc-home').style.padding = '0'
+        setTimeout(()=>{
+            document.querySelector('.logo-home').src = ''
+        },100) 
+        document.querySelector('.esc-home').style.transition = '.3s'
+
+        document.querySelector('.esc-proj').style.fontSize = '0'
+        document.querySelector('.esc-proj').style.width = '0'
+        document.querySelector('.esc-proj').style.padding = '0'
+        setTimeout(()=>{
+            document.querySelector('.logo-projetos').src = ''
+        },100) 
+        document.querySelector('.esc-proj').style.transition = '.3s'
+
         sanduiche.style.transition = '1s'
         menu.style.transition = '1s'
     }
@@ -13,6 +30,23 @@ function menu(sanduiche){
         menu.style.width = '520px'
         menu.style.height = '580px'
         sanduiche.src = '../src/image/close-line.svg'
+        
+        document.querySelector('.esc-home').style.fontSize = '18pt'
+        document.querySelector('.esc-home').style.width = '190px'
+        document.querySelector('.esc-home').style.padding = '10px'
+        setTimeout(()=>{
+            document.querySelector('.logo-home').src = '../src/image/home-2-line.svg'
+        },100)
+        document.querySelector('.esc-home').style.transition = 'width 1s, font-size 1s, background-color .3s'
+
+        document.querySelector('.esc-proj').style.fontSize = '18pt'
+        document.querySelector('.esc-proj').style.width = '190px'
+        document.querySelector('.esc-proj').style.padding = '10px'
+        setTimeout(()=>{
+            document.querySelector('.logo-projetos').src = '../src/image/folder-user-line.svg'
+        },100)
+        document.querySelector('.esc-proj').style.transition = 'width 1s, font-size 1s, background-color .3s'
+
         sanduiche.style.transition = '1s'
         menu.style.transition = '1s'
     }
@@ -27,12 +61,16 @@ function tradeButton(button){
         sobreMim.style.display = 'flex'
         redesSociais.style.display = 'none'
         button.classList.add('bttn-ativo')
+        document.querySelector('.svg-sobre').src = '../src/image/user-3-fill.svg'
+        document.querySelector('.svg-redes').src = '../src/image/message-2-line.svg'
     }
     else{
         document.querySelector('.sobre').classList.remove('bttn-ativo')
         sobreMim.style.display = 'none'
         redesSociais.style.display = 'flex'
         button.classList.add('bttn-ativo')
+        document.querySelector('.svg-redes').src = '../src/image/message-2-fill.svg'
+        document.querySelector('.svg-sobre').src = '../src/image/user-3-line.svg'
     }
 
 
